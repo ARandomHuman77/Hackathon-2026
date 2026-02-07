843cea568ceeefa4acafb0503707e5b9dedfe047
SaraV <sara.volk@mail.mcgill.ca>
2026-02-07 14:09:15 -0500

    Add files via upload
    Blank screen

---
+++ hackathon.py
@@ -0,0 +1,27 @@
+import pygame, sys
+from pygame.locals import QUIT
+
+WIDTH = 400
+HEIGHT = 400
+
+WHITE = (255, 255, 255)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Testing')
+
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(60)
+
+    # Fill the screen with the background color... this will cover any previous drawings...
+    screen.fill(WHITE)
+
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit() # Exits the program gracefully without freezing
+            sys.exit()
+
+
+    pygame.display.flip()
